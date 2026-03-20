801c61f10fab4fe94a5fbed1f0b1cbc4518161c2
JP3000 <2691967523@qq.com>
2026-03-20 16:12:19 +0800

    update video level9

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,4 @@ export const TOTAL_LEVELS = 12;
 export const STUDENT_ID_REGEX = /^[a-z]{2}\d{6}$/;
 export const FAST_PASS_STUDENT_ID = "ab999999";
 
-export const SAMPLE_VIDEO_URL =
-  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+export const SAMPLE_VIDEO_URL = "/video/level9.mp4";
